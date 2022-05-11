9c779abfd1f9158583af26bc4f09e5cc997c2eba
Jenny Gritti <100284575+jennygritti@users.noreply.github.com>
2022-05-11 08:52:59 -0300

    implementacao get post put del de postagem

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -9,15 +15,72 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class InicioComponent implements OnInit {
 
+  postagem: Postagem = new Postagem()
+  listaPostagem: Postagem[]
+
+  tema: Tema = new Tema
+  listaTemas: Tema[]
+  idTema: number
+
+  user: Usuario = new Usuario()
+  idUsuario = environment.id
+
   constructor(
-    private router: Router
+    private router: Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService,
+    private authService: AuthService
   ) { }
 
   ngOnInit() {
-    // if(environment.token == ""){
-    //   alert('Sua seção expirou, faça o login novamente')
-    //   this.router.navigate(["/entrar"])
-    // }
+    window.scroll(0,0)
+
+    if(environment.token == ""){
+      alert('Sua seção expirou, faça o login novamente')
+      this.router.navigate(["/entrar"])
+    }
+    this.authService.refreshToken()
+    this.getAllTemas()
+    this.getAllPostagem()
+  }
+
+  getAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[]) =>{
+      this.listaTemas = resp
+    })
+  }
+
+  findByIdTema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema) =>{
+      this.tema = resp
+    })
+  }
+
+  getAllPostagem(){
+    this.postagemService.getAllPostagem().subscribe((resp: Postagem[])=>{
+      this.listaPostagem = resp
+    })
+  }
+
+  findByIdUsuario(){
+    this.authService.getByIdUsuario(this.idUsuario).subscribe((resp: Usuario) =>{
+      this.user = resp
+    })
+  }
+
+  publicar(){
+    this.tema.id = this.idTema
+    this.postagem.tema = this.tema
+
+    this.user.id = this.idUsuario
+    this.postagem.usuario = this.user
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem) =>{
+      this.postagem = resp
+      alert('Postagem realizada com sucesso!')
+      this.postagem = new Postagem()
+      this.getAllPostagem()
+    })
   }
 
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -21,6 +21,10 @@ export class AuthService {
     return this.http.post<Usuario>('http://localhost:8080/usuario/cadastrar', usuario)
   }
 
+  getByIdUsuario(id: number): Observable<Usuario>{
+    return this.http.get<Usuario>(`http://localhost:8080/usuario/${id}`, this.token)
+  }
+
   logado(){
     let ok: boolean = false
 
@@ -28,5 +32,15 @@ export class AuthService {
       ok = true
     }
     return ok
+  }
+
+  token ={
+    headers: new HttpHeaders().set('Authorization', environment.token)
+}
+
+  refreshToken(){
+    this.token = {
+      headers: new HttpHeaders().set('Authorization', environment.token)
+    }
   }
 }
